34d02e5eafed96a3b1f03da9fa8e09a3db78b190
chucky-golden <chukschibyke92@gmail.com>
2024-01-14 12:42:27 +0100

    compression

--- src/express-app.js
+++ src/express-app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const basicRoutes = require('./routes/basicRoutes')
 
 module.exports = async (app) => {
     app.use(cors())
 
+    app.use(bodyParser.json({ limit: '20mb' }));
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
 
