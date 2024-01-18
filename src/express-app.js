6d44627b16b355bc175ccbdd28dd22056398886e
chucky-golden <chukschibyke92@gmail.com>
2024-01-18 05:40:27 +0100

    new updates

--- src/express-app.js
+++ src/express-app.js
@@ -1,13 +1,11 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const basicRoutes = require('./routes/basicRoutes')
 
 module.exports = async (app) => {
     app.use(cors())
 
-    app.use(bodyParser.json({ limit: '20mb' }));
-    app.use(express.json())
+    app.use(express.json({ limit: '10mb' }));
     app.use(express.urlencoded({ extended: true }))
 
     app.use('', basicRoutes)
